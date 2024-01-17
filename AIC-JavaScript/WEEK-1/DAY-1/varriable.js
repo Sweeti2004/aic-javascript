5c598e3bf1496a7fa43cbfbba8cd92aa4b6e54ee
Sweeti2004 <sweeti754940@gmail.com>
2024-01-17 10:05:05 +0530

    day-1

---
+++ AIC-JavaScript/WEEK-1/DAY-1/varriable.js
@@ -0,0 +1,19 @@
+// Types of Variable in js-1.)Automatically,2.)Var,3.)let,4.)const
+// Automatically
+x = 5;
+y = 6;
+z = x + y;
+console.log("sum="+z)
+// let
+let num1 = 11;
+let num2 = 6;
+let output = num1-num2;
+console.log("difference="+output)
+// var
+var a = 35;
+var b = 4;
+console.log("remainder="+a%b)
+// Const
+const p = 5;
+const q = 6;
+console.log("product="+p*q)
