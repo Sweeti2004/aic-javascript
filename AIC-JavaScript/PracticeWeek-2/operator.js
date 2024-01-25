84a5d563c672c5990ba086beb9c3ee72907a1560
Sweeti2004 <sweeti754940@gmail.com>
2024-01-25 22:08:57 +0530

    Week2-day1

---
+++ AIC-JavaScript/PracticeWeek-2/operator.js
@@ -0,0 +1,47 @@
+// Write a program to perform all the arithmetic operations [except increment and 
+// decrement operators] of JavaScript of any two numbers stored in the variables num1 
+// and num2. Also, print the results to the console
+let number1=54;
+let number2=7;
+//Arithmetic Operator
+console.log("Sum of",number1,"and",number2,"=",number1+number2);//Sum
+console.log("Difference of",number1,"and",number2,"=",number1-number2);//Difference
+console.log("Product of",number1,"and",number2,"=",number1*number2);//product
+console.log("Division of",number1,"and",number2,"=",number1/number2);//division
+console.log("Remainder of",number1,"and",number2,"=",number1%number2);//Remainder
+     
+
+//Write a program to demonstrate the results of comparison operators. Note that both the 
+//truth and false condition for each operator must be specified.
+    // Comparison Operators
+var num1 = 10;
+var num2 = 20;
+var num3 = 10;
+var str1 = "10";
+var str2 = "20"
+// 1. Equal to
+var result = num1 == num2; // false
+var result = num1 == num3; // true
+// 2. Strict Equal
+var result = num1 === num3; // true
+var result = num1 === str1; // false
+// 3. Not equal
+var result = num1 != num2; // true
+var result = num1 != num3; // false
+// 4. Strict Not Equal
+var result = num1 !== num3; // false
+var result = num1 !== str1; // true
+// 5. Greater than
+var result = num1 > num3; // false
+var result = num2 > num3; // true
+// 6. Greater than or Equal to
+var result = num1 >= num3; // true
+var result = num2 >= num3; // true
+// 7. Lesser than
+var result = num1 < num3; // false
+var result = num2 < num3; // false
+// 8. Lesser than or Equal to
+var result = num1 <= num3; // true
+var result = num2 <= num3; // false
+        
+         
---
+++ AIC-JavaScript/PracticeWeek-2/variable.js
@@ -0,0 +1,35 @@
+//Store two integers in two variables x and y. Print the sum of the two
+let a=50;
+let b=45;
+let sum=a+b;
+console.log("Sum of",a,"and",b,"=",sum);
+//Store two integers in two variables x and y. Print the product of the two
+let x=80;
+let y=23;
+console.log("Product of",x,"and",y,"=",x*y);
+//Calculate the average of 5 subjects
+let math=54;
+let science=98;
+let english=87;
+let hindi=85;
+let socialScience=78;
+let average=(math+science+english+hindi+socialScience)/5;
+console.log("Average =",average);
+//Calculate Area of a Circle, square, rectangle, triangle
+let r=4;
+console.log("Area of Circle =",3.14*r*r);
+console.log("Area of Square =",r*r);
+let p=7;
+let q=9;
+console.log("Area of Rectangle =",p*q);
+console.log("Area of Triangle =",1/2*p*q);
+//Calculate the simple interest
+let  principalAmount=1000;
+let rate=5;
+let time=6;
+let simpleIntrest=(principalAmount*rate*time)/100;
+console.log("Simple Intrest =",simpleIntrest);
+// Calculate the circumference of circle
+console.log("Circumference of Circle =",2*3.14*r);
+
+
