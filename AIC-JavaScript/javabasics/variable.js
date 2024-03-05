838d06cafbbefbc2a9337c61e3ad1d6572ee08f2
Sweeti2004 <sweeti754940@gmail.com>
2024-03-05 23:16:02 +0530

    variable

---
+++ AIC-JavaScript/javabasics/variable.js
@@ -0,0 +1,37 @@
+const accountId=144321
+let accountName="Sweeti"
+console.log(accountName);
+var accountEmail="sweeti4567@gmail.com"
+accountCity="Purnea"
+let accountState
+//accountId=34  //not allowed
+accountName="Swati"
+accountEmail="swati7890@gmail.com"
+accountCity="Ambala"
+console.table([accountId,accountEmail,accountName,accountCity,accountState])
+
+//Some important points
+"use stricts";//treate all js code as newer version
+//alert(3+4)//we are using nodejs,not browser
+
+console.log(50+4); console.log("Sweeti+Beauti")//Code redability decreases
+
+console.log(5
+    -4)//code readibility should be high
+
+//number =>2 to power 53
+let age=18;
+//bigint
+//string=>" "
+let s="Beautiful"
+//boolean=>true/false
+let isLoggedIn=true;
+//null=>standlon value
+let temp=null;
+//undefined=>
+let state;
+//symbol=>unique
+//object
+console.log(typeof"cutee");
+console.log(typeof null);//object
+console.log(typeof undefined);//undefined
