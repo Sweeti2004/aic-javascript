e41354ce364fc94f00529d28a264c940d87018f9
Sweeti2004 <sweeti754940@gmail.com>
2024-03-07 02:24:18 +0530

    conversion

---
+++ AIC-JavaScript/javabasics/conversion.js
@@ -0,0 +1,66 @@
+let score=33
+console.log(typeof score);
+console.log(typeof (score));
+
+let marks="55"
+console.log(typeof(marks));
+let valueInNumber=Number(marks)
+console.log(typeof(valueInNumber));
+
+let marks1="55abc"
+let valueInNumber1=Number(marks1)
+console.log(typeof(valueInNumber));
+console.log(valueInNumber1);
+
+//"55"=>55
+//"333rge"=>NaN
+//true=>1; false=>0;
+
+let isLoggedIn=1
+let booleanIsLoggedIn=Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
+
+//1=>true; 0=>false
+//""=>false
+//"Sweeti"=>true
+
+let num=33
+let stringNum=String(num)
+console.log(stringNum);
+console.log(typeof(stringNum));
+
+//**********************Operations*********************/
+let value=5
+let negValue=-value
+console.log(negValue);
+
+console.log(2+2);
+console.log(2-2);
+console.log(2*2);
+console.log(2**3);
+console.log(2%2);
+console.log(2/2);
+
+let str1="Hey!"
+let str2=" Sweeti"
+let str3=str1+str2
+console.log(str3);
+
+console.log("1"+2);//12
+console.log(1+"2");//12
+console.log("1"+2+2);//122
+console.log(1+2+"2");//32
+
+
+console.log(+true);//1
+console.log(+"");//0
+
+let numb1,numb2,numb3
+numb1=numb2=numb3=8+7
+
+let gameCounter=100
+
+console.log(gameCounter++);//100
+console.log(++gameCounter);//102
+console.log(gameCounter--);//102
+console.log(--gameCounter);//100
